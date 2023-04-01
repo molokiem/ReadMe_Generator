684ad97300c47c57cbade590437451bc1d398809
molokiem <126109483+molokiem@users.noreply.github.com>
2023-03-31 21:26:44 -0400

    add first of js code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,85 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  // TITLE
+  {
+    type: "input",
+    name: "title",
+    messgae: "What is the title of the project?",
+  },
+  // DESCRIPTION
+  {
+    type: "input",
+    name: "description",
+    messgae: "Provide a description of the project.",
+  },
+  // INSTALLATION
+  {
+    type: "input",
+    name: "install",
+    messgae: "How do you install the project?",
+  },
+  // USAGE
+  {
+    type: "input",
+    name: "usage",
+    messgae: "How do you use this project?",
+  },
+  // LICENSE
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please select a license applicable to this project.",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+  },
+  // CONTRIBUTORS
+  {
+    type: "input",
+    name: "contributors",
+    message: "Please list any contributors. (Use GitHub usernames)",
+    default: "",
+  },
+  // TEST
+  {
+    type: "input",
+    name: "test",
+    messgae: "How do you text the project?",
+  },
+//   QUESTIONS
+  {
+    type: "input",
+    name: "username",
+    messgae: "What is youe github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    messgae: "What is your github email?",
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  var content = generateMarkdown(data);
+  fs.writeFile(fileName, content, function (error) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("File written successfully");
+  });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() 
+    inquirer.prompt(questions).then(function (data) {
+        var fileName = "README.md";
+        writeToFile(fileName, data);
+    });
+
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'BSD') {
+    return [![License: BSD](https://img.shields.io/badge/License-BSD_3--Clause-red.svg)](https://opensource.org/license/bsd-3-clause)'
+  }
+  if (license === 'MIT') {
+    return [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/license/mit)'
+  }
+  if (license === 'GPL') {
+    return [![License: GPL](https://img.shields.io/badge/License-GPLv3-yellow.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  {
+    if (license === 'none') {
+      retern ''
+    } 
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
